d7d09c1bed35a03841b0796a7647509d19dd015a
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-12 22:00:25 +1000

    Create different actions for BookmarksController

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,4 +2,20 @@ class BookmarksController < ApplicationController
   def index
     render json: { message: "Welcome to Bookmarks App" }
   end
+
+  def show
+    render json: { message: "Showing bookmarks #{params[:id]}" }
+  end
+
+  def create
+    render json: { message: "Create a bookmark" }
+  end
+
+  def update
+    render json: { message: "Update bookmarks #{params[:id]}" }
+  end
+
+  def destroy
+    render json: { message: "Delete bookmarks #{params[:id]}" }
+  end
 end
