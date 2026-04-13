9bbde1e121756b3f6e0b84a302895974c64c3049
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-13 23:44:25 +1000

    - Insert format to an URL - URL must be unique

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
 class Bookmark < ApplicationRecord
   validates :title, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, format: { with: /\Ahttps?:\/\//, message: "must start with http:// or https://" }, uniqueness: true
 end
