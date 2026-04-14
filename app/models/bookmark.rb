39255eb9aa3affe4df0b3ff07ace88133be0121a
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-14 14:39:57 +1000

    Update association on bookmark model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true, format: { with: /\Ahttps?:\/\//, message: "must start with http:// or https://" }, uniqueness: true
+
+  has_many :bookmark_tags
+  has_many :tags, through: :bookmark_tags
 end
