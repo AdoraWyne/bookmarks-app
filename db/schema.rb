48bf5bffe903530753991735822990b3329d2364
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-14 14:35:12 +1000

    Run the migration and schema got updated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_12_120943) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_13_135301) do
+  create_table "bookmark_tags", force: :cascade do |t|
+    t.integer "bookmark_id", null: false
+    t.datetime "created_at", null: false
+    t.integer "tag_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bookmark_id"], name: "index_bookmark_tags_on_bookmark_id"
+    t.index ["tag_id"], name: "index_bookmark_tags_on_tag_id"
+  end
+
   create_table "bookmarks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
     t.string "url"
   end
+
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookmark_tags", "bookmarks"
+  add_foreign_key "bookmark_tags", "tags"
 end
